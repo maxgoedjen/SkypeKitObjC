e6fc4ea975394f1730eb3ef007ae5734b43025dd
Max Goedjen <max.goedjen@gmail.com>
2013-06-02 01:44:58 -0700

    STPriveledgedTask

--- SkypeKitRuntimeLauncher/main.m
+++ SkypeKitRuntimeLauncher/main.m
@@ -7,24 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "STPrivilegedTask.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSString* path = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
-        NSTask* runtime = [[NSTask alloc] init];
+        STPrivilegedTask* runtime = [[STPrivilegedTask alloc] init];
         [runtime setLaunchPath:path];
-        
-        NSPipe* errorPipe = [NSPipe pipe];
-        [runtime setStandardError:errorPipe];
-        NSFileHandle* errorHandle = [errorPipe fileHandleForReading];
-        
+                
         [runtime launch];
         
-        NSData* data = nil;
-        
-        while ([data length] == 0) {
-            data = [errorHandle availableData];
-        }
     }
     return 0;
 }
